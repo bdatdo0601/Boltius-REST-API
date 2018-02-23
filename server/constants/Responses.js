f389bed448ada4b8cacaeffc94679253bd165780
Dat Do <dod2@wit.edu>
2018-02-23 00:25:31 -0500

    add constatn

---
+++ server/constants/Responses.js
@@ -0,0 +1,11 @@
+module.exports = Object.freeze({
+    "401": {
+        description: "Unauthenticated Request",
+    },
+    "403": {
+        description: "Unauthorized Request",
+    },
+    "400": {
+        description: "Bad Request",
+    },
+});
